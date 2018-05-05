798248a339a78999e76e270dd8b1ec92a483f671
Gustavo Morentin <gmorentin@ucol.mx>
2018-05-05 16:15:59 -0500

    add counter_cache and order by votes count

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_05_181953) do
+ActiveRecord::Schema.define(version: 2018_05_05_204651) do
 
   create_table "topics", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "votes_count", default: 0
   end
 
   create_table "votes", force: :cascade do |t|
